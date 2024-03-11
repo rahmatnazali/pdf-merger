9d969be767501e668eee5a0938d12e3020531697
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-03-11 09:01:22 +0700

    update merge.py

--- merge.py
+++ merge.py
@@ -4,8 +4,10 @@ from pypdf import PdfWriter
 
 merger = PdfWriter()
 
-for pdf in sorted(os.listdir('source/process')):
-    merger.append(f"source/process/{pdf}")
+for pdf in sorted(os.listdir('source')):
+    pdf_path = f"source/{pdf}"
+    print(f"Merging: {pdf_path}")
+    merger.append(pdf_path)
 
 merger.write('output/merged.pdf')
 merger.close()
